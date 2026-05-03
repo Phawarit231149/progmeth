cf5a29b3111383c69568239e8ea788ced8b469a0
Phawarit <phavarisyatniyom@gmail.com>
2026-05-03 11:22:42 +0700

    m

--- src/main/java/gui/GameController.java
+++ src/main/java/gui/GameController.java
@@ -146,6 +146,7 @@ public class GameController extends StackPane {
 
         // แสดงผลตรงกลางหน้าจอหลัก
         popupStage.showAndWait();
+        timer.play();
     }
 
     // ── GRID ─────────────────────────────────────────────
